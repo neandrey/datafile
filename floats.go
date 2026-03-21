8179f878972939a416f66ccbd3e21a364cd8b985
ne_andrey <sorokinac@gmail.com>
2026-03-22 01:46:53 +0500

    add slice data value

--- floats.go
+++ floats.go
@@ -8,20 +8,20 @@ import (
 )
 
 // GetFloats читает значение float64 из каждой строки файла
-func GetFloats(fileName string) ([3]float64, error) {
-	var numbers [3]float64
+// и добавляет значения в сегмент
+func GetFloats(fileName string) ([]float64, error) {
+	var numbers []float64
 	file, err := os.Open(fileName)
 	if err != nil {
 		return  numbers, err
 	}
-	i := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		numbers[i], err = strconv.ParseFloat(scanner.Text(), 64)
+		number, err := strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
 			return  numbers, err
 		}
-		i++
+		numbers = append(numbers, number)
 	}
 
 	err = file.Close()
